6d39751b62142a18b6030b7d77a5df919c6fd304
ITSASWINV <aswinvezhakkodan73@gmail.com>
2023-05-27 16:57:02 +0530

    blog app website
    its a blog web

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react'
+import { Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+
+export default function Home() {
+
+  const [blog, setBlog] = useState([]);
+
+
+
+
+  const getData = async () => {
+    const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const jsonData = await data.json();
+    setBlog(jsonData)
+  }
+
+
+
+
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+
+
+
+  return (
+    <div>
+      <Container>
+
+
+
+        <Row>
+          {
+            blog.map((item, index) => {
+              return (
+                <Col key={index}>
+                  <Card className='mt-3' style={{ width: '18rem', maxHeight: '100rem', height: '33rem' }}>
+                  <Card.Img variant="top" src="https://i.pinimg.com/originals/f7/d1/4c/f7d14cdbe744edb59aa6af2b3be76f96.jpg" />
+                    <Card.Body>
+                      <Card.Title>{item.title}</Card.Title>
+                      <Card.Text>{item.body}</Card.Text>
+                      <Button variant="primary">Explore More</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              )
+            })
+          }
+        </Row>
+      </Container>
+    </div>
+  )
+}
